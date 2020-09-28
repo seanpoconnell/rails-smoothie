39f967fd6032f22ccfcb179cc248353f52a98ce3
sean oconnell <seanoconnell96@gmail.com>
2020-09-28 22:40:43 +0100

    models completed

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :smoothie
   belongs_to :ingredient
+
+  validates :description, :smoothie, presence: true
+  validates :smoothie, uniqueness: { scope: :ingredient }
 end
--- app/models/smoothie.rb
+++ app/models/smoothie.rb
@@ -1,3 +1,5 @@
 class Smoothie < ApplicationRecord
-  has_many :doses
+  validates :name, presence: true, uniqueness: true
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
 end
