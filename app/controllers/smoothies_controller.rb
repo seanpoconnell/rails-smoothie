5f6a748e3b17963523c0a46ceea5fa92dfd67a98
sean oconnell <seanoconnell96@gmail.com>
2020-10-02 15:42:17 +0100

    smoothie controller complete

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,7 +1,29 @@
 class SmoothiesController < ApplicationController
-
   def index
     @smoothies = Smoothie.all
   end
 
+  def show
+    @smoothie = Smoothie.find(params[:id])
+    @does = Dose.new
+  end
+
+  def new
+    @smoothie = Smoothie.new
+  end
+
+  def create
+    @smoothie = Smoothie.new(smoothie_params)
+    if @smoothie.save
+      redirect_to smoothie_path(@smoothie)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def smoothie_params
+    params.require(:smoothie).permit(:name)
+  end
 end
