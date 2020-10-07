b63cc0a4c2b538b66f2a95ab29d68cfdbeb31617
sean oconnell <seanoconnell96@gmail.com>
2020-10-07 17:06:34 +0100

    navbar spacing on show page so it doesn't cover anything

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def should_i_display_the_navbar_background?
+    if controller_name == "smoothies" && action_name == "index"
+      true
+    else
+      false
+    end
+  end
 end
