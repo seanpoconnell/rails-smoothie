41286a596a8b67f2d0bddddff7fe9f649154ae6e
sean oconnell <seanoconnell96@gmail.com>
2020-10-11 23:03:58 +0100

    add smoothie prep

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'smoothies#home'
+  get "users/:id/edit", to: "users#edit", as: :edit_profile
   resources :smoothies, only: [:home, :show, :new, :create] do
     resources :doses, only: [:create, :new]
   end
